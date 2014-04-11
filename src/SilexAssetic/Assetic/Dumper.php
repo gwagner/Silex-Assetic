1c770a31075e5340ce81cbe7a4095e9e996d8db0
Geoffrey Wagner <wageof@gmail.com>
2014-04-11 12:21:21 -0500

    Update Dumper.php
    Fixes a condition where assets get parsed multiple times.  For example:
    
    When loading files in debug mode, the dumper would parse my scss file in "combine" mode and then re-parse it again in "un-combined" mode creating wasted CPU and developer time.  Doesn't seem like a lot but we are talking a big difference on slower computers.

--- src/SilexAssetic/Assetic/Dumper.php
+++ src/SilexAssetic/Assetic/Dumper.php
@@ -108,20 +108,22 @@ class Dumper
                 $formula = $am->getFormula($name);
             }
 
-            $this->writer->writeAsset($asset);
+            if (isset($formula[2])) {
 
-            if (!isset($formula[2])) {
-                continue;
-            }
+                $debug   = isset($formula[2]['debug'])   ? $formula[2]['debug']   : $am->isDebug();
+                $combine = isset($formula[2]['combine']) ? $formula[2]['combine'] : null;
 
-            $debug   = isset($formula[2]['debug'])   ? $formula[2]['debug']   : $am->isDebug();
-            $combine = isset($formula[2]['combine']) ? $formula[2]['combine'] : null;
+                if (null !== $combine ? !$combine : $debug) {
+                    /** @var $leaf \Assetic\Asset\FileAsset */
+                    foreach ($asset as $leaf) {
+                        $this->writer->writeAsset( $leaf );
+                    }
 
-            if (null !== $combine ? !$combine : $debug) {
-                foreach ($asset as $leaf) {
-                    $this->writer->writeAsset($leaf);
+                    continue;
                 }
             }
+
+            $this->writer->writeAsset($asset);
         }
     }
 }
